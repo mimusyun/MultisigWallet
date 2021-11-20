4aebad5bb67f71c87394c6575c9f7031f25eac93
Mimura Shunichiro <mimuramimusyun@gmail.com>
2021-11-20 14:34:56 +0100

    NIJ: add modifier onlyOwners

--- MultiSigWallet.sol
+++ MultiSigWallet.sol
@@ -21,6 +21,11 @@ contract MultiSigWallet {
     mapping(address => uint) balance;
     mapping(address => bool) isApproved;
     
+    modifier onlyOwners() {
+        require(isApproved[msg.sender], "Address not owner");
+        _;
+    }
+    
     constructor(address[] memory _ownerAddrs, uint _numRequiredApproval) {
         require(_numRequiredApproval > 0, "Number of approvals needs to be more than zero");
         require(_numRequiredApproval <= _ownerAddrs.length, "The number of required approvals cannot exceed the number of approvers");
@@ -35,14 +40,12 @@ contract MultiSigWallet {
         balance[msg.sender] += msg.value;
     }
     
-    function createTransferRequest(uint amount, address payable recipient) public {
-        require(isApproved[msg.sender], "Address not owner");
+    function createTransferRequest(uint amount, address payable recipient) public onlyOwners {
         require(msg.sender != recipient, "Don't transfer money to yourself");
         transferReqs.push(TransferRequest(msg.sender, recipient, amount, false));
     } 
     
-    function approveTransferRequest(uint id) public {
-        require(isApproved[msg.sender], "Address not owner");
+    function approveTransferRequest(uint id) public onlyOwners {
         require(!transferReqs[id].complete, "Transfer already complete");
         require(balance[transferReqs[id].fromAddr] >= transferReqs[id].amount, "Balance not sufficient");
         transferApprovalCnt[transferReqs[id].fromAddr][transferReqs[id].toAddr][id] += 1;
